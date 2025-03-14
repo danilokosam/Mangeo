b6233296970ee63b57db5602034da1fc2d4dc610
Daniel Duarte <danielduartemora27@gmail.com>
2025-03-14 14:35:16 +0100

    Configuración inicial de eslint y prettier

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import eslint from "vite-plugin-eslint"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslint({
+      failOnError: false,
+      failOnWarning: false
+    })
+  ]
 })
